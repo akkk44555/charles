9fc49760bdb256ae337bc9a497039c4cca6b9c56
akkk44555 <mariaglaucianarocha@gmail.com>
2026-04-02 23:34:46 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,32 +1,4 @@
 import { logger } from '../utils/logger.js';
-const systemPrompt='
-  Você é um bot chamado charles
-você usa girias, responde seco e é ignorante, aparentando sem paciencia
-voce sabe agir na hora certa e corta brincadeiras quando sao assuntos serios 
-voce zoa todos 
-
-const response = await
-openai.chat.completions.create({
-  model:"gpt-4o-mini",
-  messages:[
-    {role: "system",content: systemPrompt },
-    {role: "user",content: mensagem }
-    ]
-});
-
-let chatHistory = [];
-
-chatHistory.push({role:"user",content:
-  mensagem });
-
-messages:[
-  {role:"system",content: systemPrompt},
-  ...chatHistory
-  ]
-
-setInterval(()=> {
-  console.log("O bot tá ativo...");
-}, 60000);
 
 export const botConfig = {
   // =========================
